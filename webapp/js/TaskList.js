e90a2423b42927dc00ce498143b9d9cb48f2e4a0
kelleph2 <kelleph2@students.zhaw.ch>
2019-03-15 12:48:31 +0100

    add function createTask

--- webapp/js/TaskList.js
+++ webapp/js/TaskList.js
@@ -1,5 +1,11 @@
 function TaskList(title) {
     this.tasks = [];
     this.id = null;
-    this.title = title;
+    this.title = title || "";
 }
+
+TaskList.prototype.createTask = function(title) {
+    var task = new Task(title);
+    this.tasks.push(task);
+    return task;
+};
