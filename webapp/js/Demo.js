3bd33ee9baffb4e0af24a2f7858a9b1bc36b9434
baervan1 <baervan1@students.zhaw.ch>
2019-04-11 16:14:12 +0200

    Added tests

--- webapp/js/Demo.js
+++ webapp/js/Demo.js
@@ -4,24 +4,11 @@ var taskList;
 
 $(function() {
 
-    taskList = new TaskList();
-    taskList.createTask("Setup todo list");
-    taskList.createTask("Buy milk");
-    taskList.createTask("Read recipe");
-    taskList.createTask("Invite guests");
-    taskList.tasks[0].done = true;
-
-    console.log('Application successfully initialized');
-
-    if (window.location.hash.length > 16) {
-        TaskList.load(window.location.hash.substring(1), function(tl) {
-            taskList = tl;
-            $('#tasks').html(taskList.render());
-        });
-    } else {
         taskList = new TaskList();
-        taskList.createTask('');
+        taskList.createTask("Setup todo list");
+        taskList.createTask("Buy milk");
+        taskList.createTask("Read recipe");
+        taskList.createTask("Invite guests");
         $('#tasks').html(taskList.render());
-    }
 
 });
