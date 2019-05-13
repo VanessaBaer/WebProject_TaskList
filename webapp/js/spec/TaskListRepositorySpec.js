8a551709bdd9bf78142b3ea32dc0bd0d1b8abd12
baervan1 <baervan1@students.zhaw.ch>
2019-05-13 13:02:03 +0200

    Changed tests.

--- webapp/js/spec/TaskListRepositorySpec.js
+++ webapp/js/spec/TaskListRepositorySpec.js
@@ -1,16 +1,28 @@
 
 describe("Save", () => {
 
-    it("Save tasklist", () => {
+    it("Save tasklist", (done) => {
         var taskList = new TaskList('My Tasks', '#tasks');
         taskList.createTask("Foo");
         taskList.createTask("Bar");
         taskList.tasks[1].setDone(true);
-        TaskListRepository.save(taskList);
-        var expectedTasksJson = [
-            {title: "Foo", done: false},
-            {title: "Bar", done: true},
-        ];
-        expect(TaskListRepository.findTasksJsonById()).toEqual(expectedTasksJson);
+        TaskListRepository.saveTaskListById(taskList,() => {
+            function callbackId(id) {
+
+                TaskListRepository.findTasksById(id, () => {
+                    function callback(data) {
+
+                        var expectedTasksJson = [
+                            {title: "Foo", done: false},
+                            {title: "Bar", done: true},
+                        ];
+
+                        expect(data).toEqual(expectedTasksJson);
+                        done();
+                    }
+
+                });
+            }
+            });
     });
 });
